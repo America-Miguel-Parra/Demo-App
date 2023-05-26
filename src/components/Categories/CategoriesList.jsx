6a8744b0aa721faa66db53c504ad3a47b069a5b0
America-Miguel-Parra <isc19350307@gmail.com>
2023-05-25 21:00:04 -0600

    Correcion con use effect

--- src/components/Categories/CategoriesList.jsx
+++ src/components/Categories/CategoriesList.jsx
@@ -1,10 +1,15 @@
-const CategoriesList = ({ categories = [] }) => {
+const CategoriesList = ({ categories = [], setCategories }) => {
+    const deleteCategory = (category) => {
+        const newList = categories.filter((cat) => cat !== category)
+        setCategories([...newList])
+    }
+
     return (
         <div>
             <ol>
                 {
                     categories.map((category) => (
-                        <li key={category}>
+                        <li onClick={() => deleteCategory(category)} key={category}>
                             {category}
                         </li>
                     ))
@@ -13,4 +18,5 @@ const CategoriesList = ({ categories = [] }) => {
         </div>
     )
 }
+
 export default CategoriesList
